fcba135ed00275ae6a1d58772e28d90dcded4ee2
Ezra <njerukithinji872@gmail.com>
2024-03-20 10:03:18 +0300

    addedd url pages and dynamic routing

--- base/views.py
+++ base/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 
-def home(request):
-    return render(request, "home.html")
 
-def room(request):
-    return render(request,"room.html")
+rooms =[
+    {'id':1, "name":"python"},
+    {'id':2, "name":"Javascript"},
+    {'id':3, "name":"java"},
+    {'id':4, "name":"golang"},
+]
+
+def home(request):
+    context = {'rooms':rooms}
+    return render(request, "base/home.html", context)
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room':room}
+    return render(request,"base/room.html", context)
 
 
